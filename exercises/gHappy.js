f9c696027553fa100c0d553ec132f86169cf8ed9
Jonthan <cheeyongjc@gmail.com>
2021-12-01 17:21:10 -0600

    passed test for ghappy

--- exercises/gHappy.js
+++ exercises/gHappy.js
@@ -1,13 +1,22 @@
-module.exports.run = function(str) {
-/*
-A lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
-Return true if all the g's in the given string are happy, otherwise return false.
+module.exports.run = function (str) {
+	/*
+	A lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
+	Return true if all the g's in the given string are happy, otherwise return false.
 
-	Example:
-		function("xxggxx") => true
-		function("xxgxx") => false
+		Example:
+			function("xxggxx") => true
+			function("xxgxx") => false
 
-        Write your code below the comment.
-*/
+			Write your code below the comment.
+	*/
+	if (str === '') return false;
+	if (str.length <= 2 && str !== 'gg') return false;
+	for (let i = 1; i < str.length; i++) {
+		if (str[i] === 'g' && !(str[i - 1] === 'g' || str[i + 1] === 'g')) return false;
+	}
+	return true;
+	//attempted regex but didn't work for each test
+	// const regex = '[^g]g[^g]';
+	// return str.match(regex) === null;
 
 };
