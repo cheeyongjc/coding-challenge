b7dff9be3934cb796635d1594e1c3f1fb7bb705a
Jonthan <cheeyongjc@gmail.com>
2021-12-01 22:38:07 -0600

    finished all problems

--- exercises/csvToArray.js
+++ exercises/csvToArray.js
@@ -1,19 +1,33 @@
-module.exports.run = function(csv){
-/*
-	A stringified CSV file will be passed into this function.
-	Parse the string so it is an array of objects and return the array.
-	The object properties are the header of the csv file, and the very first row of the csv file are the headers.
+module.exports.run = function (csv) {
+	/*
+		A stringified CSV file will be passed into this function.
+		Parse the string so it is an array of objects and return the array.
+		The object properties are the header of the csv file, and the very first row of the csv file are the headers.
 
-	Example
+		Example
 
-	function ('FirstName,LastName,Age\nDan,Tran,29\nDon,Tran,25\nJasmine,Tran,13') =>
-		[
-			{FirstName: Dan, LastName: Tran, Age: 29},
-			{FirstName: Don, LastName: Tran, Age: 25},
-			{FirstName: Jasmine, LastName: Tran, Age: 13},
-		]
+		function ('FirstName,LastName,Age\nDan,Tran,29\nDon,Tran,25\nJasmine,Tran,13') =>
+			[
+				{FirstName: Dan, LastName: Tran, Age: 29},
+				{FirstName: Don, LastName: Tran, Age: 25},
+				{FirstName: Jasmine, LastName: Tran, Age: 13},
+			]
 
-	Write your code below the comment.
-*/
+		Write your code below the comment.
+	*/
 
+	//example has backslashes which is correct for linebreak. test file has forward slashes to indicate line breaks which is incorrect. LET BRYCE KNOW TESTS ARE WRONG!!!!!!!
+	let lines = csv.split('/n');
+	let objectProperties = lines[0].split(',');
+	let arr = [];
+
+	for (i = 1; i < lines.length; i++) {
+		let o = {};
+		let currentLineValues = lines[i].split(',');
+		for (j = 0; j < objectProperties.length; j++) {
+			o[objectProperties[j]] = currentLineValues[j];
+		}
+		arr.push(o)
+	}
+	return arr;
 };
